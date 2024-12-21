7128f2ff125c10c53adf7f1af0dc13c4e807c1f2
KUKKU-PANDA <kukkupanda1@gmail.com>
2024-12-21 07:56:37 +0530

    Add files via upload

---
+++ plugins/yta.js
@@ -0,0 +1,53 @@
+const config = require('../config');
+const { cmd } = require('../command');
+const { ytsearch, ytmp3, ytmp4 } = require('@dark-yasiya/yt-dl.js'); 
+
+
+cmd({
+    pattern: "play3",
+    alias: ["audio2","song2","ytsong"],
+    react: "🎶",
+    desc: "Download Youtube song",
+    category: "main",
+    use: '.song < Yt url or Name >',
+    filename: __filename
+},
+async(conn, mek, m,{ from, prefix, quoted, q, reply }) => {
+try{
+
+if(!q) return await reply("Please give me Yt url or Name")
+	
+const yt = await ytsearch(q);
+if(yt.results.length < 1) return reply("Results is not found !")
+
+let yts = yt.results[0]  
+const ytdl = await ytmp3(yts.url)
+		
+let ytmsg = `╭━━━〔 *𝕶𝖚𝖐𝖐𝖚 𝕻𝖆𝖓𝖉𝖆 𝕸𝕯* 〕━━━┈⊷
+┃▸╭───────────
+┃▸┃๏ *MUSIC DOWNLOADER*
+┃▸└───────────···๏
+╰────────────────┈⊷
+╭━━❐━⪼
+┇🐼 *Tital* -  ${yts.title}
+┇🐼 *Duration* - ${yts.timestamp}
+┇🐼 *Views* -  ${yts.views}
+┇🐼 *Author* -  ${yts.author.name} 
+┇🐼 *Link* -  ${yts.url}
+╰━━❑━⪼
+> *© ᴋᴜᴋᴋᴜ ᴘᴀɴᴅᴀ ᴍᴅ ᴠ1*`
+// SEND DETAILS
+await conn.sendMessage(from, { image: { url: yts.thumbnail || yts.image || '' }, caption: `${ytmsg}`}, { quoted: mek });
+
+// SEND AUDIO TYPE
+await conn.sendMessage(from, { audio: { url: ytdl.download.url }, mimetype: "audio/mpeg" }, { quoted: mek })
+
+// SEND DOC TYPE
+await conn.sendMessage(from, { document: { url: ytdl.download.url }, mimetype: "audio/mpeg", fileName: ytdl.result.title + '.mp3', caption: `> *© ᴋᴜᴋᴋᴜ ᴘᴀɴᴅᴀ ᴍᴅ ᴠ1*` }, { quoted: mek })
+
+
+} catch (e) {
+console.log(e)
+reply(e)
+}}
+)
